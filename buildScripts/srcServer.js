83f4135cb7fdd126fa840965663353cb47073210
Tom Nelson <tnelson@legacy.com>
2017-07-13 12:37:51 -0500

    implemented mock api using json-server and mock data

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -24,6 +24,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
   publicPath: config.output.publicPath
 }));
 
+app.get('/users', function(req, res) {
+  // Hard coding for simplicity.  Pretend this hits a real database
+  res.json([
+    {"id": 1, "firstName": "Bob", "LastName": "Smith", "email": "bob@gmail.com"},
+    {"id": 2, "firstName": "Tammy", "LastName": "Norton", "email": "tnorton@gmail.com"},
+    {"id": 3, "firstName": "Tina", "LastName": "Lee", "email": "lee.tina@gmail.com"}
+  ]);
+});
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
